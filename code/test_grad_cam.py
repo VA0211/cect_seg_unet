9849177fd3fe54df6f7e6e1a9634cf8c30218ee0
VA0211 <ldva0211@gmail.com>
2025-09-15 12:41:40 +0700

    gradcam se

--- code/test_grad_cam.py
+++ code/test_grad_cam.py
@@ -197,7 +197,7 @@ def Inference(FLAGS):
     os.makedirs(gradcam_save_path, exist_ok=True)
 
     net = net_factory(net_type=FLAGS.model, in_chns=1, class_num=FLAGS.num_classes)
-    save_mode_path = os.path.join(f"/kaggle/input/cect_model_weight/pytorch/default/{FLAGS.labeled_num}", f'{FLAGS.model}_best_model.pth')
+    save_mode_path = os.path.join(f"/kaggle/input/cect_model_weight/pytorch/default/{FLAGS.labeled_num}", f'{FLAGS.model}_se_best_model.pth')
     print(save_mode_path)
     net.load_state_dict(torch.load(save_mode_path))
     net.eval()
