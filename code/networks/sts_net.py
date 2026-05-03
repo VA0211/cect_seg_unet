c65f9dc5846d59ee6d874d7a5608977987ffbe7c
VA0211 <ldva0211@gmail.com>
2026-05-03 21:51:12 +0700

    add decoder

--- code/networks/sts_net.py
+++ code/networks/sts_net.py
@@ -16,6 +16,7 @@ class ESCA(nn.Module):
         self.gap = nn.AdaptiveAvgPool2d(1)
         self.conv = nn.Sequential(
             nn.Conv1d(1, 1, kernel_size=kernel_size, padding=(kernel_size - 1) // 2, bias=False),
+            nn.ReLU(inplace=True), # <-- Added non-linear activation
             nn.Conv1d(1, 1, kernel_size=kernel_size, padding=(kernel_size - 1) // 2, bias=False)
         )
         self.sigmoid = nn.Sigmoid()
@@ -80,9 +81,33 @@ class ASPP(nn.Module):
         x = self.relu(x)
         return self.dropout(x)
 
+class DecoderBlock(nn.Module):
+    """
+    Standard Decoder Block with upsampling, skip connection concatenation, and convolutions.
+    """
+    def __init__(self, in_channels, skip_channels, out_channels):
+        super(DecoderBlock, self).__init__()
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels + skip_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x, skip):
+        x = self.up(x)
+        # Handle potential dimension mismatches due to pooling rounding
+        if x.shape[2:] != skip.shape[2:]:
+            x = F.interpolate(x, size=skip.shape[2:], mode='bilinear', align_corners=True)
+        x = torch.cat([x, skip], dim=1)
+        return self.conv(x)
+    
 class STSNet(nn.Module):
     """
-    STS-Net (ResNeXt50 + ESCA + DeepLabV3)
+    STS-Net (ResNeXt50 + ESCA + DeepLabV3 ASPP + U-Net Decoder)
     """
     def __init__(self, in_channels=1, num_classes=2):
         super(STSNet, self).__init__()
@@ -94,7 +119,10 @@ class STSNet(nn.Module):
         if in_channels != 3:
             resnext.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
             
+        # ENCODER (5 RA Blocks)
         self.encoder1 = nn.Sequential(resnext.conv1, resnext.bn1, resnext.relu, resnext.maxpool)
+        self.esca1 = ESCA(64) # Added 5th ESCA block here
+        
         self.encoder2 = resnext.layer1
         self.esca2 = ESCA(256)
         
@@ -107,28 +135,46 @@ class STSNet(nn.Module):
         self.encoder5 = resnext.layer4
         self.esca5 = ESCA(2048)
         
-        # DeepLabV3 Decoder (ASPP)
+        # BOTTLENECK (DeepLabV3 ASPP)
         self.aspp = ASPP(in_channels=2048, out_channels=256)
+        
+        # DECODER (4 DB Blocks with Skip Connections)
+        # ASPP outputs 256. e4 skip is 1024.
+        self.db1 = DecoderBlock(in_channels=256, skip_channels=1024, out_channels=512)
+        self.db2 = DecoderBlock(in_channels=512, skip_channels=512, out_channels=256)
+        self.db3 = DecoderBlock(in_channels=256, skip_channels=256, out_channels=128)
+        self.db4 = DecoderBlock(in_channels=128, skip_channels=64, out_channels=64)
         
         # Final segmentation head
         self.classifier = nn.Sequential(
-            nn.Conv2d(256, 256, 3, padding=1, bias=False),
-            nn.BatchNorm2d(256),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
-            nn.Conv2d(256, num_classes, 1)
+            nn.Conv2d(64, num_classes, kernel_size=1)
         )
 
     def forward(self, x):
-        input_size = x.size()[2:]
+        input_size = x.shape[2:]
         
-        e1 = self.encoder1(x)
+        # Encoder Pathway
+        e1 = self.esca1(self.encoder1(x))
         e2 = self.esca2(self.encoder2(e1))
         e3 = self.esca3(self.encoder3(e2))
         e4 = self.esca4(self.encoder4(e3))
         e5 = self.esca5(self.encoder5(e4))
         
+        # ASPP Bottleneck
         aspp_out = self.aspp(e5)
-        out = self.classifier(aspp_out)
         
+        # Decoder Pathway with Skip Connections
+        d1 = self.db1(aspp_out, e4)
+        d2 = self.db2(d1, e3)
+        d3 = self.db3(d2, e2)
+        d4 = self.db4(d3, e1)
+        
+        # Final Classification
+        out = self.classifier(d4)
+        
+        # Final upsampling to match original input size (e1 was stride 2)
         out = F.interpolate(out, size=input_size, mode='bilinear', align_corners=True)
         return out
